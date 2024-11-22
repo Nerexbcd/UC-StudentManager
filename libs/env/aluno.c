ec3a11dde840a5f6edf5bc7d8cd4a8b13684ae2a
inescarragoso <carragoso.ines@gmail.com>
2024-11-22 12:21:15 +0000

    typedef struct aluno

---
+++ libs/env/aluno.c
@@ -0,0 +1,17 @@
+#include "aluno.h"
+
+
+//Define a estrutura aluno, de modo a organizar a informação de cada aluno.
+//Parâmetro ocupado indica se há um aluno ou não nessa posição da estrutura; permite a eliminação de alunos.
+typedef struct aluno
+{
+    int ocupado;
+    int codigo;
+    int n_matriculas;
+    int ects_concluidos;
+    int ano_curso;
+    float media_atual;
+    char nome[100];
+    char nacionalidade[100];
+    char data_n[10];
+}ALUNO;
