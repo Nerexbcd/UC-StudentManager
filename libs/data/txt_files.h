35f2272e4834bf67cea83ffde93aff82f464f59e
Nerexbcd <me@nerexbcd.dev>
2024-11-27 16:02:49 +0000

    Added some comments

--- libs/data/txt_files.h
+++ libs/data/txt_files.h
@@ -12,16 +12,23 @@ typedef struct txtFile {
 
 
 /* Initialize txt file data array */
+/* It Prepares the file to be used */
 txtFile txt_files_init(char *path);
 
 /* Load txt files in to Program */
+/* It reads the file and stores the data in the data array */
 void txt_load_file(txtFile *txt);
 
 /* Unload txt files from Program */
+/* It frees the memory allocated for the data array */
 void txt_unload_file(txtFile *txt);
 
+/* Get txt file data */
+/* It returns an array with each line one the file*/
 char** txt_get_data(txtFile txt);
 
+/* Get txt file size */
+/* It returns the number of lines in the file */
 size_t txt_get_size(txtFile txt);
 
 #endif
--- libs/utils/string_util.h
+++ libs/utils/string_util.h
@@ -2,6 +2,7 @@
 #define STRING_UTIL_H_INCLUDED
 
 /* Split a String */
+/* It splits a string by a separator and returns an array with each part. Also can return the size of parts the original string was separeted */
 char** str_split(char str[], const char separator, size_t *size);
 
 #endif
