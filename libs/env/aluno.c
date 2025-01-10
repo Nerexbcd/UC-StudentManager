435ca65bf49a8c642ee2d67141a0b4c886ee1b85
inescarragoso <carragoso.ines@gmail.com>
2025-01-10 14:00:41 +0000

    jkasha

--- libs/env/aluno.c
+++ libs/env/aluno.c
@@ -21,7 +21,7 @@
 
 ALUNO * criar_lista(int size_alunos) {
 
-    STUDENT *lista_estudantes; //estrutura com informacao dos alunos
+    ALUNO * lista_estudantes; //estrutura com informacao dos alunos
     lista_estudantes = malloc(sizeof(STUDENT)*((size_alunos)+1));
 
     if (sizeof(lista_estudantes)==0) {
